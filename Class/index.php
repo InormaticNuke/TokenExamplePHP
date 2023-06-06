8d2a0088ba5a399055fd36307a50721fafc6422f
InformaticNuke <InformaticNuke@gmail.com>
2023-06-06 19:35:15 -0400

    FirstTest
    Testing 1

---
+++ Class/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function functionName(){
+    //ExampleCode
+}
+
+function MSG(){
+    echo "<h1>TestMessage</h1>";
+}
+
+MSG();
+
+function showNames($name){
+    echo "My name is: ".$name."<br>";
+}
+
+showNames("Manuel");
+showNames("Jamon");
+showNames("Queso");
+showNames("Juan");
+showNames("Perez");
+
+?>
