8171c397be963871488196cf2c5060beb0de3094
InformaticNuke <InformaticNuke@gmail.com>
2023-06-06 20:56:16 -0400

    Session Recovery example
    Example Data

---
+++ Sessions/data.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>Session Data</h2>
+    <?php
+
+    echo "Favorite colour: ".$_SESSION["favcolor"]."<br>";
+    echo "Favorite colour: ".$_SESSION["favanimal"]."<br>";
+
+    ?>
+    
+</body>
+</html>
